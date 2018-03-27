8dfcc646030de3e1c283e9f4900adb4e36a0943d
dmitriev-z <zahandoc@gmail.com>
2018-03-27 23:37:44 +0300

    changes in log_parse.py

--- log_parse/log_parse.py
+++ log_parse/log_parse.py
@@ -1,17 +1,13 @@
 # -*- encoding: utf-8 -*-
 import re
-import datetime
+from datetime import datetime
 import collections
 main_pattern = re.compile(r'^\[(?P<request_date>\d{1,2}/?[a-zA-Z]+/?\d{1,4} \d{2}:\d{2}:\d{2})?\] ?\"?'
                           r'(?P<request_type>[A-Z]*)? ?(?P<request>[\w:/.\-=?&#% ]*[^ A-Z]) ?(?P<protocol>[A-Z]*)?/'
                           r'(?P<protocol_version>[\d.]*)?\"? ?(?P<response_code>\d{3})? ?(?P<response_time>\d*)?$')
-date_pattern = re.compile(r'^(?P<day>\d{1,2})/(?P<month>[a-zA-Z]*)/(?P<year>\d{4}) (?P<hour>\d{1,2}):'
-                          r'(?P<minute>\d{1,2}):(?P<second>\d{1,2})$')
 request_pattern = re.compile(r'^(?P<scheme>[a-z]*)://(?P<auth_params>|[\w:-]*@)(?P<host>[\w\-.]*):?(?P<port>\d*)?'
                              r'(?P<url_path>|/[\w\-/.]*)\??(?P<parameters>[\w\-=&.%]*)?#?(?P<anchor>[\w\-=&.]*)$')
-auth_pattern = re.compile(r'^(?P<login>[\w-]*)?:?(?P<password>[\w-]*)?@?$')
-months = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'july': 7, 'august': 8,
-          'september': 9, 'october': 10, 'november': 11, 'december': 12}
+auth_pattern = re.compile(r'^(?P<login>[\w-]*):?(?P<password>[\w-]*)@?$')
 
 
 def parse_log_string(log_string) -> dict:
@@ -21,13 +17,8 @@ def parse_log_string(log_string) -> dict:
         pass
     else:
         parsed_log_string = main_pattern.fullmatch(log_string).groupdict()
-        parsed_log_string['request_date'] = parse_date_string(parsed_log_string['request_date'])
         parsed_log_string['request'] = parse_request_string(parsed_log_string['request'])
         return parsed_log_string
-
-
-def parse_date_string(date_string):
-    return date_pattern.fullmatch(date_string).groupdict()
 
 
 def parse_request_string(request_string):
@@ -38,14 +29,6 @@ def parse_request_string(request_string):
 
 def parse_auth_params_string(auth_params_string):
     return auth_pattern.fullmatch(auth_params_string).groupdict()
-
-
-def make_datetime_from_log_date(date):
-    for month, month_number in months.items():
-        if date['month'].lower() in month:
-            date['month'] = month_number
-            break
-    return datetime.datetime(date['year'], date['month'], date['day'], date['hour'], date['minute'], date['second'])
 
 
 def make_url_statistics_from_logs(logs, count=False, response=False):
@@ -75,29 +58,26 @@ def parse(
         for line in r.readlines():
             line = line.strip('\n')
             parsed_log = parse_log_string(line.strip('\n'))
-            if parsed_log:
-                if ignore_files:
-                    file_pattern = re.compile(r'\.[a-z]*')
-                    if file_pattern.findall(parsed_log['request']['url_path']):
-                        continue
-                if ignore_urls:
-                    url = parsed_log['request']['host'] + parsed_log['request']['url_path']
-                    if url in ignore_urls:
-                        continue
-                if start_at:
-                    log_datetime = make_datetime_from_log_date(parsed_log['date'])
-                    if log_datetime < start_at:
-                        continue
-                if stop_at:
-                    log_datetime = make_datetime_from_log_date(parsed_log['date'])
-                    if log_datetime > start_at:
-                        continue
-                if request_type:
-                    if parsed_log['request']['request_type'].lower() != request_type.lower():
-                        continue
-                if ignore_www:
-                    parsed_log['request']['host'] = parsed_log['request']['host'].replace('www.', '')
-                parsed_logs.append(parsed_log)
+            if not parsed_log:
+                continue
+            if ignore_files:
+                file_pattern = re.compile(r'\.[a-z]*')
+                if file_pattern.findall(parsed_log['request']['url_path']):
+                    continue
+            if ignore_urls:
+                url = parsed_log['request']['host'] + parsed_log['request']['url_path']
+                if url in ignore_urls:
+                    continue
+            if start_at or stop_at:
+                log_datetime = datetime.strptime(parsed_log['request_date'], '%d/%b/%Y %H:%M:%S')
+                if log_datetime < start_at or log_datetime > stop_at:
+                    continue
+            if request_type:
+                if parsed_log['request']['request_type'].lower() != request_type.lower():
+                    continue
+            if ignore_www:
+                parsed_log['request']['host'] = parsed_log['request']['host'].replace('www.', '')
+            parsed_logs.append(parsed_log)
     if not slow_queries:
         urls_statistics = make_url_statistics_from_logs(parsed_logs, count=True)
     else:
