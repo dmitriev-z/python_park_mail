86975bf438fe339526cc385c7e56779fb0efe6ad
dmitriev-z <zahandoc@gmail.com>
2018-05-20 15:45:02 +0300

    fixes according to comments

--- task_tracker/database_filler.py
+++ task_tracker/database_filler.py
@@ -1,11 +1,10 @@
 import sqlite3
 import random
-import string
 
 NUMBER_OF_WORKERS = 20
 
 
-class DatabaseFiller():
+class DatabaseFiller:
     def __init__(self):
         self._connection = sqlite3.connect("task_tracker.db")
         self._cursor = self._connection.cursor()
@@ -39,14 +38,10 @@ class DatabaseFiller():
     @staticmethod
     def _load_last_names():
         last_names = []
-        with open("last_names.txt", 'r') as r:
+        with open("last_names.txt", "r") as r:
             for line in r.readlines():
                 last_names.append(line.strip())
         return last_names
-
-    @staticmethod
-    def _create_id():
-        return ''.join(random.choices(list(string.ascii_letters + string.digits), k=32))
 
     def _clean_up(self):
         self._cursor.execute("""DROP TABLE IF EXISTS tasks;""")
@@ -56,7 +51,7 @@ class DatabaseFiller():
 
     def _create_tasks_table(self):
         tasks = """
-            CREATE TABLE tasks(task_id VARCHAR(32) NOT NULL PRIMARY KEY,
+            CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
             task_name VARCHAR(255),
             status VARCHAR(11),
             worker VARCHAR(32),
@@ -66,36 +61,41 @@ class DatabaseFiller():
 
     def _create_workers_table(self):
         workers = """
-            CREATE TABLE workers(worker_id VARCHAR(32) NOT NULL PRIMARY KEY,
+            CREATE TABLE workers(worker_id INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name VARCHAR(255),
             last_name VARCHAR(255));
             """
         self._cursor.execute(workers)
 
     def fill_tasks_table(self):
-        tasks = [{'id': self._create_id(), 'name': 'clean', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'clean bedroom', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'clean bathroom', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'clean kitchen', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'cook cake', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'cook', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'cook dinner', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'cook soup', 'status': 'in queue', 'worker': '', 'parent_task': ''},
-                 {'id': self._create_id(), 'name': 'cook meat', 'status': 'in queue', 'worker': '', 'parent_task': ''}]
+        tasks = [{"name": "clean", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "clean bedroom", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "clean bathroom", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "clean kitchen", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "cook cake", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "cook", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "cook dinner", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "cook soup", "status": "in queue", "worker": "", "parent_task": ""},
+                 {"name": "cook meat", "status": "in queue", "worker": "", "parent_task": ""}]
+        sql_query = """INSERT INTO tasks (task_name, status, worker, parent_task) VALUES """
         for task in tasks:
-            task_id, task_name, status, worker, parent_task = task.values()
-            sql_command = """INSERT INTO tasks VALUES('{}', '{}', '{}', '{}', '{}');"""\
-                .format(task_id, task_name, status, worker, parent_task)
-            self._cursor.execute(sql_command)
+            task_name, status, worker, parent_task = task.values()
+            values = """("{}", "{}", "{}", "{}")""".format(task_name, status, worker, parent_task)
+            sql_query = sql_query + values + ", "
+        sql_query = sql_query.rstrip(", ")
+        sql_query += ";"
+        self._cursor.execute(sql_query)
 
     def fill_workers_table(self):
+        sql_query = """INSERT INTO workers (first_name, last_name) VALUES """
         for _ in range(self._number_of_workers):
-            worker_id = self._create_id()
             first_name = random.choice(random.choice(self._names))
             last_name = random.choice(self._last_names)
-            sql_command = """INSERT INTO workers VALUES('{}', '{}', '{}');"""\
-                .format(worker_id, first_name, last_name)
-            self._cursor.execute(sql_command)
+            values = """("{}", "{}")""".format(first_name, last_name)
+            sql_query = sql_query + values + ", "
+        sql_query = sql_query.rstrip(", ")
+        sql_query += ";"
+        self._cursor.execute(sql_query)
 
 
 base = DatabaseFiller()
--- task_tracker/task_tracker.py
+++ task_tracker/task_tracker.py
@@ -1,6 +1,4 @@
 import sqlite3
-import random
-import string
 
 
 class TaskTracker:
@@ -9,12 +7,7 @@ class TaskTracker:
         self._cursor = self._connection.cursor()
 
     def __del__(self):
-        self._connection.commit()
         self._connection.close()
-
-    @staticmethod
-    def _create_id():
-        return ''.join(random.choices(list(string.ascii_letters + string.digits), k=32))
 
     def view_all_tasks(self):
         select = """SELECT * FROM tasks"""
@@ -34,53 +27,63 @@ class TaskTracker:
             print("Worker id: {}\nFirst name: {}\nLast name: {}\n".
                   format(worker_id, first_name, last_name))
 
-    def add_new_task(self, task_name:str):
-        task_id = self._create_id()
-        sql_command = """INSERT INTO tasks VALUES('{}', '{}', '{}', '', '');""" \
-            .format(task_id, task_name, 'in queue')
-        self._cursor.execute(sql_command)
+    def add_new_task(self, task_name: str):
+        sql_query = """INSERT INTO tasks (task_name, status, worker, parent_task) VALUES("{}", "{}", "", "");"""\
+            .format(task_name, "in queue")
+        self._cursor.execute(sql_query)
+        self._connection.commit()
+        sql_query = """SELECT * FROM tasks ORDER BY task_id DESC LIMIT 1"""
+        self._cursor.execute(sql_query)
+        result = self._cursor.fetchone()
+        task_id, task_name, status, worker, parent_task = result
         print("New task created!\nTask id: {}\nTask name: {}\n".format(task_id, task_name))
 
-    def add_new_worker(self, first_name:str, last_name:str):
-        worker_id = self._create_id()
-        sql_command = """INSERT INTO workers VALUES('{}', '{}', '{}');""" \
-            .format(worker_id, first_name, last_name)
-        self._cursor.execute(sql_command)
+    def add_new_worker(self, first_name: str, last_name: str):
+        sql_query = """INSERT INTO workers (first_name, last_name) VALUES("{}", "{}");""" \
+            .format(first_name, last_name)
+        self._cursor.execute(sql_query)
+        self._connection.commit()
+        sql_query = """SELECT * FROM workers ORDER BY worker_id DESC LIMIT 1"""
+        self._cursor.execute(sql_query)
+        result = self._cursor.fetchone()
+        worker_id, first_name, last_name = result
         print("New worker created!\nWorker id: {}\nWorker first name: {}\nWorker last name: {}\n"
               .format(worker_id, first_name, last_name))
 
     def take_task_to_work(self, task_id, worker_id):
-        sql_command = """SELECT * FROM tasks WHERE task_id == '{}';""".format(task_id)
-        self._cursor.execute(sql_command)
+        sql_query = """SELECT * FROM tasks WHERE task_id == "{}";""".format(task_id)
+        self._cursor.execute(sql_query)
         result = self._cursor.fetchall()[0]
         task_id, task_name, status, worker, parent_task = result
-        if status == 'in queue':
-            sql_command = """UPDATE tasks SET worker = '{}', status = '{}' WHERE task_id == '{}';""" \
-                .format(worker_id, 'in progress', task_id)
-            self._cursor.execute(sql_command)
+        if status == "in queue":
+            sql_query = """UPDATE tasks SET worker = "{}", status = "{}" WHERE task_id == "{}";""" \
+                .format(worker_id, "in progress", task_id)
+            self._cursor.execute(sql_query)
+            self._connection.commit()
             print("Task with id '{}' now marked as 'in progress' by worker with id '{}'".format(task_id, worker_id))
             self._operate_nested_tasks(task_id, worker_id, to_work=True)
         else:
             print("Task with id '{}' already 'in progress' by worker with id '{}'".format(task_id, worker))
 
     def finish_task(self, task_id):
-        sql_command = """SELECT * FROM tasks WHERE task_id == '{}';""".format(task_id)
-        self._cursor.execute(sql_command)
+        sql_query = """SELECT * FROM tasks WHERE task_id == "{}";""".format(task_id)
+        self._cursor.execute(sql_query)
         result = self._cursor.fetchall()[0]
         task_id, task_name, status, worker, parent_task = result
-        if status == 'in progress':
-            sql_command = """UPDATE tasks SET status = 'done' WHERE task_id == '{}';""".format(task_id)
-            self._cursor.execute(sql_command)
+        if status == "in progress":
+            sql_query = """UPDATE tasks SET status = "done" WHERE task_id == "{}";""".format(task_id)
+            self._cursor.execute(sql_query)
+            self._connection.commit()
             print("Task with id '{}' now marked as 'done'".format(task_id))
             self._operate_nested_tasks(task_id, finish=True)
-        elif status == 'in queue':
+        elif status == "in queue":
             print("Cant finish task which is not in progress")
-        elif status == 'done':
+        elif status == "done":
             print("Task is already finished")
 
     def _operate_nested_tasks(self, parent_task_id, worker_id=None, to_work=False, finish=False):
-        sql_command = """SELECT * FROM tasks WHERE parent_task == '{}';""".format(parent_task_id)
-        self._cursor.execute(sql_command)
+        sql_query = """SELECT * FROM tasks WHERE parent_task == "{}";""".format(parent_task_id)
+        self._cursor.execute(sql_query)
         results = self._cursor.fetchall()
         if not results:
             return
@@ -92,21 +95,22 @@ class TaskTracker:
                 self.finish_task(child_task_id)
 
     def get_task_status(self, task_id):
-        sql_command = """SELECT * FROM tasks WHERE task_id == '{}';""".format(task_id)
-        self._cursor.execute(sql_command)
+        sql_query = """SELECT * FROM tasks WHERE task_id == "{}";""".format(task_id)
+        self._cursor.execute(sql_query)
         result = self._cursor.fetchall()[0]
         task_id, task_name, status, worker, parent_task = result
         print("Task with id '{}' is '{}'".format(task_id, status))
 
     def make_task_nested(self, child_task, parent_task):
-        sql_command = """SELECT * FROM tasks WHERE task_id == '{}';""".format(child_task)
-        self._cursor.execute(sql_command)
+        sql_query = """SELECT * FROM tasks WHERE task_id == "{}";""".format(child_task)
+        self._cursor.execute(sql_query)
         result = self._cursor.fetchall()[0]
         task_id, task_name, status, worker, current_parent_task = result
-        if current_parent_task == '':
-            sql_command = """UPDATE tasks SET parent_task = '{}' WHERE task_id == '{}';"""\
+        if current_parent_task == "":
+            sql_query = """UPDATE tasks SET parent_task = "{}" WHERE task_id == "{}";"""\
                 .format(parent_task, child_task)
-            self._cursor.execute(sql_command)
+            self._cursor.execute(sql_query)
+            self._connection.commit()
             print("Task with id '{}' now nested to task with id '{}'".format(child_task, parent_task))
         elif current_parent_task != parent_task:
             print("Task with id '{}' is nested to another task with id '{}'".format(child_task, current_parent_task))
